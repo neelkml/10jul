24c974e05e7e39774b17239413d8edf7539f0cb0
neelkml <neelkamalsingh145@gmail.com>
2017-07-10 12:37:21 +0530

    Add files via upload

---
+++ table.java
@@ -0,0 +1,52 @@
+package neel;
+
+public class table {
+void printTable(int n)
+{
+	for(int i=1;i<=10;i++)
+	{
+		System.out.println(n*i);
+	
+	try
+	{
+		Thread.sleep(200);
+	}
+	catch(Exception ex)
+	{
+		System.out.println(ex);
+	}
+	
+}
+}
+}
+class Mythread1 extends Thread
+{
+	table t;
+	Mythread1(table t){
+		this.t=t;
+	}
+	public void run() {
+		t.printTable(5);
+	}
+	class Mythread2 extends Thread
+	{
+		table t;
+		Mythread2(table t){
+			this.t=t;
+		}
+		public void run() {
+			t.printTable(100);
+		}
+	public class TestSync
+	{
+		public void main(String args[]) {
+			table obj=new table();
+			Mythread1 t1=new Mythread1(obj);
+			Mythread2 t2=new Mythread2(obj);
+			t1.start();
+			t2.start();
+		}
+	}
+
+}
+}
